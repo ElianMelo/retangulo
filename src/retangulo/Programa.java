b210d7623d0a37fd55e4dbd0b3a1cab11a9d145d
ElianMelo <elianmelomorais@gmail.com>
2020-05-20 19:31:59 -0300

    Programa finalizado

--- src/retangulo/Programa.java
+++ src/retangulo/Programa.java
@@ -10,6 +10,7 @@ public class Programa {
 		Scanner sc = new Scanner(System.in);
 		
 		double largura, altura;
+		int menu;
 		
 		System.out.print("Digite a largura do retangulo: ");
 		largura = sc.nextDouble();
@@ -25,6 +26,54 @@ public class Programa {
 		while(altura <= 0.0) {
 			System.out.print("O valor deve ser positivo. Tente novamente: ");
 			altura = sc.nextDouble();
+		}
+		
+		System.out.println();
+		
+		System.out.println("MENU:");
+		System.out.println("1 - Mostrar area");
+		System.out.println("2 - Mostrar perimetro");
+		System.out.println("3 - Sair");
+		System.out.print("Digite uma opcao: ");
+		menu = sc.nextInt();
+		
+		System.out.println();
+		
+		if (menu == 1) {
+			System.out.printf("AREA = %.1f%n", altura * largura);
+			System.out.println();
+		} else if (menu == 2) {
+			System.out.printf("PERIMETRO = %.1f%n", (2 * altura) + (2 * largura));
+			System.out.println();
+		} else if (menu == 3) {
+			System.out.println("FIM DO PROGRAMA!");
+		} else {
+			System.out.println("OPÇÃO INVALIDA");
+			System.out.println();
+		}
+		
+		while(menu != 3) {
+			System.out.println("MENU:");
+			System.out.println("1 - Mostrar area");
+			System.out.println("2 - Mostrar perimetro");
+			System.out.println("3 - Sair");
+			System.out.print("Digite uma opcao: ");
+			menu = sc.nextInt();
+			
+			System.out.println();
+			
+			if (menu == 1) {
+				System.out.printf("AREA = %.1f%n", altura * largura);
+				System.out.println();
+			} else if (menu == 2) {
+				System.out.printf("PERIMETRO = %.1f%n", (2 * altura) + (2 * largura));
+				System.out.println();
+			} else if (menu == 3) {
+				System.out.println("FIM DO PROGRAMA!");
+			} else {
+				System.out.println("OPÇÃO INVALIDA");
+				System.out.println();
+			}
 		}
 		
 		sc.close();
